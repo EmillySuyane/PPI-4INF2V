b0be7b33ef50875e45caf5a60e0c79ddfde75838
EmillySuyane <emillysuyane936@gmail.com>
2025-05-30 13:37:22 +0000

    aula 29/05/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyText } from "./components/MyText";
+import { MyTextList } from "./components/MyTextList";
 
 export default function App() {
-  const texts = [
-    { title: "Meu título 1", text: "Meu texto 1",},
-    { title: "Meu título 2", text: "Meu texto 2",},
-    { title: "Meu título 3", text: "Meu texto 3",},
-    { title: "Meu título 4", text: "Meu texto 4",},
-    { title: "Meu título 5", text: "Meu texto 5",},
-    { title: "Meu título 6",text: "Meu texto 6",},
-  ];
 
   return (
-    //React Fragment
+    // React Fragment
     <>
-    {texts.map((text, index) => (
-        <MyText key={index} title={`${index + 1}. ${text.title}`}>
-          {text.text}
-        </MyText>
-      ))}
+      <MyTextList />
     </>
   );
 }
--- src/components/MyTextList.jsx
+++ src/components/MyTextList.jsx
@@ -1,24 +1,25 @@
-import styles from ".MyTextList.module.css";
+import styles from "./MyTextList.module.css";
 import { MyText } from "./MyText";
 
-export default function App() {
+export function MyTextList() {
   const texts = [
-    { title: "Meu título 1", text: "Meu texto 1",},
-    { title: "Meu título 2", text: "Meu texto 2",},
-    { title: "Meu título 3", text: "Meu texto 3",},
-    { title: "Meu título 4", text: "Meu texto 4",},
-    { title: "Meu título 5", text: "Meu texto 5",},
-    { title: "Meu título 6",text: "Meu texto 6",},
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
   ];
 
   return (
-    //React Fragment
-    <>
-    {texts.map((text, index) => (
-        <MyText key={index} title={`${index + 1}. ${text.title}`}>
-          {text.text}
-        </MyText>
-      ))}
-    </>
+    <div className={styles.container}>
+      {texts.map((text, index) => {
+        return (
+          <MyText key={index} title={`${text.title} ${index + 1}`}>
+            {text.text}
+          </MyText>
+        );
+      })}
+    </div>
   );
 }
