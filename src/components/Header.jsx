850563e73cbc388ec650683b35a78434076e4300
EmillySuyane <emillysuyane936@gmail.com>
2025-07-18 17:24:05 +0000

    aula 18/07

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router";
 import styles from "./Header.module.css";
-import { Github, Instagram, Phone } from "lucide-react";
-export function Header() {
-  // Desestruturação de props
+import { ShoppingBasket } from "lucide-react";
+
+export function Header({ cart }) {
   return (
-    <header className={styles.header1}>
-      <h1>Foco, Força, Fé</h1>
+    <header className={styles.header}>
+      <Link to="/"><h1>TRJ Megastore</h1></Link>
+      <div>
+        <Link to="/cart"><ShoppingBasket size={24} /></Link>
+        <p>
+          Total $: {cart.reduce((total, product) => total + product.price, 0).toFixed(2)}
+        </p>
+      </div>
     </header>
   );
 }
